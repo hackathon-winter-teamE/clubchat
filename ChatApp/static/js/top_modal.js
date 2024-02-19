d0d8119c9ccd6b146aea995837b986023c8a6b52
tokkun2 <tetsu.shiro2500@gmail.com>
2024-02-19 21:13:06 +0900

    ログイン・新規登録モーダル用CSS追記

--- ChatApp/static/js/top_modal.js
+++ ChatApp/static/js/top_modal.js
@@ -8,18 +8,3 @@ function closeModal(modalId) {
   document.getElementById(modalId).style.display = 'none';
   document.getElementById('overlay').style.display = 'none';
 }
-
-function submitLoginForm() {
-  var email = document.getElementById('loginEmail').value;
-  var password = document.getElementById('loginPassword').value;
-  closeModal('loginModal');
-}
-
-function submitRegistrationForm() {
-  var name = document.getElementById('name').value;
-  var email = document.getElementById('registrationEmail').value;
-  var password = document.getElementById('registrationPassword').value;
-  var password = document.getElementById('registrationPassword2').value;
-  var password = document.getElementById('signup-club-serect').value;
-  closeModal('registrationModal');
-}
