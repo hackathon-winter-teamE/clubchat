71dfdbf93f631e76fb5d11fb870022df340f0df3
tokkun2 <tetsu.shiro2500@gmail.com>
2024-02-22 23:21:19 +0900

    ログインと新規登録のモーダルを廃止、ログインと新規登録のバックエンド処理を追加、リストで選択した部活のidを取得する処理を追加etc

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -11,23 +11,86 @@ app.secret_key = uuid.uuid4().hex
 app.permanent_session_lifetime = timedelta(days=30)
 
 
+# チャンネル一覧ページの表示
 @app.route('/')
 def index():
-    clubs = ['選択してください'] + dbConnect.getClubsAll()
-    return render_template('registration/top.html', clubs=clubs)
+    uid = session.get("uid")
+    if uid is None:
+        return redirect('/login')
+    # else:
+    #     channels = dbConnect.getChannelAll()
+    #     channels.reverse()
+    return render_template('test.html')
 
-@app.route('/', methods=['POST'])
-def userSignup():
-    uid = uuid.uuid4()
-    user_name = request.form.get('name')
+# ログインページの表示
+@app.route('/login')
+def login():
+    return render_template('registration/login.html')
+
+# ログイン処理
+@app.route('/login', methods=['POST'])
+def userLogin():
     email = request.form.get('email')
-    password= request.form.get('password')
-    club_id= request.form.get('club_id')
-    dbConnect.createUser(uid,user_name, email, password,club_id)
-    UserId = str(uid)
-    session['uid'] = UserId
-    return redirect('/')
+    password = request.form.get('password')
 
+    if email =='' or password == '':
+        flash('空のフォームがあります')
+    else:
+        user = dbConnect.getUser(email)
+        if user is None:
+            flash('このユーザーは存在しません')
+        else:
+            hashPassword = hashlib.sha256(password.encode('utf-8')).hexdigest()
+            if hashPassword != user["password"]:
+                flash('パスワードが間違っています！')
+            else:
+                session['uid'] = user["uid"]
+                return redirect('/')
+    return redirect('/login')
+
+# サインアップページの表示
+@app.route('/signup')
+def signup():
+    clubs =  ['選択してください'] + dbConnect.getClubs()
+    return render_template('registration/signup.html', clubs=clubs)
+
+# サインアップ処理
+@app.route('/signup', methods=['POST'])
+def userSignup():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    password1 = request.form.get('password1')
+    password2 = request.form.get('password2')
+    club_name = request.form.get('clubs')
+
+    pattern = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+
+    if name == '' or email =='' or password1 == '' or password2 == '' or club_name == '':
+        flash('空のフォームがあります')
+    elif password1 != password2:
+        flash('二つのパスワードの値が違っています')
+    elif re.match(pattern, email) is None:
+        flash('正しいメールアドレスの形式ではありません')
+    else:
+        uid = uuid.uuid4()
+        password = hashlib.sha256(password1.encode('utf-8')).hexdigest()
+        DBuser = dbConnect.getUser(email)
+        club_id = dbConnect.getClubId(club_name)
+
+        if DBuser != None:
+            flash('既に登録されているようです')
+        else:
+            dbConnect.createUser(uid, name, email, password,club_id)
+            UserId = str(uid)
+            session['uid'] = UserId
+            return redirect('/')
+    return redirect('/signup')
+
+# ログアウト
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/login')
 
 
 if __name__ == '__main__':
--- ChatApp/models.py
+++ ChatApp/models.py
@@ -7,7 +7,7 @@ class dbConnect:
         try:
             conn = DB.getConnection()
             cur = conn.cursor()
-            sql="INSERT INTO users (uid,user_name, email, password,club_id) VALUES (%s,%s,%s,%s,%s);"
+            sql="INSERT INTO users (uid,user_name, email, password,club_id) VALUES (%s, %s, %s, %s, %s);"
             cur.execute(sql,(uid,user_name, email, password,club_id))
             conn.commit()
         except Exception as e:
@@ -16,16 +16,57 @@ class dbConnect:
         finally:
             cur.close()
 
-#DBから部活をとってくる
-    def getClubsAll():
+    def getUser(email):
         try:
             conn = DB.getConnection()
             cur = conn.cursor()
-            sql = "SELECT club_name FROM clubs ORDER BY club_id ASC;"
+            sql = "SELECT * FROM users WHERE email=%s;"
+            cur.execute(sql, (email))
+            user = cur.fetchone()
+            return user
+        except Exception as e:
+            print(str(e) + 'が発生しています')
+            abort(500)
+        finally:
+            cur.close()
+
+    def getClubId(club_name):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "SELECT club_id FROM clubs WHERE club_name=%s;"
+            cur.execute(sql, (club_name,))
+            clubid = cur.fetchone()
+            return clubid
+        except Exception as e:
+            print(str(e) + 'が発生しています')
+            abort(500)
+        finally:
+            cur.close()
+
+#DBから部活をとってくる
+    def getClubs():
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "SELECT * FROM clubs ORDER BY club_id ASC;"
             cur.execute(sql)
             clubs = cur.fetchall()
-            print(clubs)
             return clubs
+        except Exception as e:
+            print(str(e) + 'が発生しています')
+            abort(500)
+        finally:
+            cur.close()
+
+    def getChannelAll():
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "SELECT * FROM channels;"
+            cur.execute(sql)
+            channels = cur.fetchall()
+            return channels
         except Exception as e:
             print(e + 'が発生しています')
             abort(500)
