937a1bca4e98bdbd62e2ca9bbdcbb4614cd5a1ae
tokkun2 <tetsu.shiro2500@gmail.com>
2024-02-24 19:46:33 +0900

    ログインしたユーザーの部活名とチャンネル一覧を追加

--- ChatApp/models.py
+++ ChatApp/models.py
@@ -30,6 +30,53 @@ class dbConnect:
         finally:
             cur.close()
 
+    # ログインしたユーザーのclub_idを取得
+    def getUserClubId(uid):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "SELECT club_id FROM users WHERE uid=%s;"
+            cur.execute(sql, (uid))
+            cid = cur.fetchone()
+            user_club_id = cid['club_id']
+            return user_club_id
+        except Exception as e:
+            print(str(e) + 'が発生しています１')
+            abort(500)
+        finally:
+            cur.close()
+
+    # ログインしたユーザーのclub_nameを取得
+    def getUserClubName(user_club_id):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "SELECT club_name FROM clubs WHERE club_id=%s;"
+            cur.execute(sql, (user_club_id))
+            ucname = cur.fetchone()
+            user_club_name = ucname['club_name']
+            return user_club_name
+        except Exception as e:
+            print(str(e) + 'が発生しています２')
+            abort(500)
+        finally:
+            cur.close()
+
+    # ログインしたユーザーと同じclub_idを持つチャンネルを取得
+    def getUserChannels(user_club_id):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "SELECT * FROM channels WHERE club_id=%s;"
+            cur.execute(sql, (user_club_id))
+            channels = cur.fetchone()
+            return channels
+        except Exception as e:
+            print(str(e) + 'が発生しています３')
+            abort(500)
+        finally:
+            cur.close()
+
 #DBから部活をとってくる
     def getClubs():
         try:
@@ -58,3 +105,4 @@ class dbConnect:
             abort(500)
         finally:
             cur.close()
+
