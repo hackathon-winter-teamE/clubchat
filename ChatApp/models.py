5e1c5f607bd35a2529c626804eaba035deafaeab
tokkun2 <tetsu.shiro2500@gmail.com>
2024-02-25 21:27:48 +0900

    チャンネル一覧等追加

--- ChatApp/models.py
+++ ChatApp/models.py
@@ -3,6 +3,7 @@ from util.DB import DB
 from flask import abort
 
 class dbConnect:
+    # ユーザー登録
     def createUser(uid, user_name, email, password,club_id):
         try:
             conn = DB.getConnection()
@@ -16,6 +17,7 @@ class dbConnect:
         finally:
             cur.close()
 
+    # usersテーブルからメールアドレスを取得
     def getUser(email):
         try:
             conn = DB.getConnection()
@@ -30,7 +32,7 @@ class dbConnect:
         finally:
             cur.close()
 
-    # ログインしたユーザーのclub_idを取得
+    # ログインユーザーのclub_idを取得
     def getUserClubId(uid):
         try:
             conn = DB.getConnection()
@@ -46,7 +48,7 @@ class dbConnect:
         finally:
             cur.close()
 
-    # ログインしたユーザーのclub_nameを取得
+    # ログインユーザーのclub_nameを取得
     def getUserClubName(user_club_id):
         try:
             conn = DB.getConnection()
@@ -62,14 +64,14 @@ class dbConnect:
         finally:
             cur.close()
 
-    # ログインしたユーザーと同じclub_idを持つチャンネルを取得
+    # ログインユーザーと同じclub_idを持つチャンネルを取得
     def getUserChannels(user_club_id):
         try:
             conn = DB.getConnection()
             cur = conn.cursor()
             sql = "SELECT * FROM channels WHERE club_id=%s;"
             cur.execute(sql, (user_club_id))
-            channels = cur.fetchone()
+            channels = cur.fetchall()
             return channels
         except Exception as e:
             print(str(e) + 'が発生しています３')
@@ -77,7 +79,81 @@ class dbConnect:
         finally:
             cur.close()
 
-#DBから部活をとってくる
+    # ログインユーザーと同じclub_idを持つ新規チャンネルを追加
+    def addChannel(uid, newChannelName, newChannelDescription, user_club_id):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "INSERT INTO channels (uid, name, abstract) VALUES (%s, %s, %s, %s);"
+            cur.execute(sql, (uid, newChannelName, newChannelDescription, user_club_id))
+            conn.commit()
+        except Exception as e:
+            print(e + 'が発生しています')
+            abort(500)
+        finally:
+            cur.close()
+
+    # 入力されたチャンネル名と同じチャンネル名を取得
+    # （ログインユーザーと同じclub_idのもののみ）
+    def getChannelByName(channel_name,user_club_id):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "SELECT * FROM channels WHERE name=%s AND club_id=%s;"
+            cur.execute(sql, (channel_name,user_club_id))
+            channel = cur.fetchone()
+        except Exception as e:
+            print(e + 'が発生しました')
+            abort(500)
+        finally:
+            cur.close()
+            return channel
+
+    # チャンネルIDを取得
+    def getChannelById(cid):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "SELECT * FROM channels WHERE id=%s;"
+            cur.execute(sql, (cid))
+            channel = cur.fetchone()
+            return channel
+        except Exception as e:
+            print(e + 'が発生しています')
+            abort(500)
+        finally:
+            cur.close()
+
+    # チャンネル更新
+    def updateChannel(uid, newChannelName, newChannelDescription, cid):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "UPDATE channels SET uid=%s, name=%s, abstract=%s WHERE id=%s;"
+            cur.execute(sql, (uid, newChannelName, newChannelDescription, cid))
+            conn.commit()
+        except Exception as e:
+            print(e + 'が発生しました')
+            abort(500)
+        finally:
+            cur.close()
+
+    # チャンネルを削除
+    def deleteChannel(cid):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "DELETE FROM channels WHERE id=%s;"
+            cur.execute(sql, (cid))
+            conn.commit()
+        except Exception as e:
+            print(e + 'が発生しています')
+            abort(500)
+        finally:
+            cur.close()
+
+
+#DBから部活一覧を取得
     def getClubs():
         try:
             conn = DB.getConnection()
